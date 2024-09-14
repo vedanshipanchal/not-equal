7a4b64145f08e2bcc21cc8e20632a5ed4ba8e58c
vedanshipanchal <vedanshipanchal@gmail.com>
2024-09-14 17:42:37 +0100

    Equals to

---
+++ main.py
@@ -0,0 +1,15 @@
+#part 1
+t= 10
+n = 10
+if t!=n:
+    print("Not equals")
+else:
+    print("equals")
+
+#part 2
+f = "Vedanshi"
+u = "Vedanshi"
+if f!=u:
+    print("not equals")
+else:
+    print("similar name")
